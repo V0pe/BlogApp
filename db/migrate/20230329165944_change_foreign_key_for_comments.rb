b217f20d3b1facc750bf0fdaee2e0c3df7900f05
V0pe <dampblew01@gmail.com>
2023-03-29 11:02:15 -0600

    add database

---
+++ db/migrate/20230329165944_change_foreign_key_for_comments.rb
@@ -0,0 +1,4 @@
+class ChangeForeignKeyForComments < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
---
+++ db/migrate/20230329165953_change_foreign_key_for_likes.rb
@@ -0,0 +1,4 @@
+class ChangeForeignKeyForLikes < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
