7ef369857e9b03f908c726c48f1d6fd417feb3c4
V0pe <dampblew01@gmail.com>
2023-03-29 16:00:03 -0600

    fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :post, counter_cache: :likes_counter
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  
+
   after_save :update_likes_counter
 
   private
