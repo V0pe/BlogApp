95e411c6ffcc8cc2944bb924ecc2d336c59f08fc
V0pe <dampblew01@gmail.com>
2023-03-30 23:24:17 -0600

    add validation for user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: :posts_counter
   has_many :likes
   has_many :comments
+  
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality : { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }  
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
