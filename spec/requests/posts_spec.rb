40461b63d454090789b825fba79c61bb108ac529
V0pe <dampblew01@gmail.com>
2023-04-06 22:32:25 -0600

    add the request test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'Posts' do
+    it 'returns http success, html content Body ,  Template' do
+      get '/users/id/posts'
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('Posts')
+      expect(response).to render_template(:index)
+    end
+
+    it 'returns http success, html Body ,  Template' do
+      get '/users/id/posts/id'
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('Post')
+      expect(response).to render_template(:show)
+    end
   end
 end
