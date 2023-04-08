4bd07bc650282955048dd5aa7e2652754085ceff
V0pe <dampblew01@gmail.com>
2023-04-07 20:23:29 -0600

    add the controller methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @posts = User.find(params[:id])
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
