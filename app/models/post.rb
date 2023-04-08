be33b92678ea9ea4f406877f7cfdf08baadc3dc7
V0pe <dampblew01@gmail.com>
2023-04-07 22:58:01 -0600

    fix errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ApplicationRecord
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
-  
+
   def user_name
     author.name
   end
@@ -18,9 +18,9 @@ class Post < ApplicationRecord
   def user_posts_counter
     author.posts_counter
   end
-  
+
   def user_id
-    author.id 
+    author.id
   end
 
   def all_comments
