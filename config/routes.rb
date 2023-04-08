6a4c30acac8e4bb0a299d7b6331e9a2bf13a7350
V0pe <dampblew01@gmail.com>
2023-04-08 09:55:13 -0600

    add new post route and template

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
-  end
+  end 
+  get '/new', to: "users#new_post"
+  post '/new', to: "users#save_post"
+  post '/addcomment/:id', to: "users#addcomment"
+  get 'addlike/:id', to: "users#addlike"
   root 'users#index'
 end
