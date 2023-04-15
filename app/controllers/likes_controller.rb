2d949f9585090b9e9423780b5463c5cedcdc0012
V0pe <dampblew01@gmail.com>
2023-04-15 08:13:38 -0600

    fix error

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,12 @@
 class LikesController < ApplicationController
- # def create
+  # def create
   #  @post = Post.find(params[:id])
-   # @like = @post.likes.new
-   # @like.post_id = @post.id
-   # @like.author_id = @user.id
-   # @like.save
-   # redirect_to "/users/#{current_user.id}/posts/#{@post.id}"
-  #end
+  # @like = @post.likes.new
+  # @like.post_id = @post.id
+  # @like.author_id = @user.id
+  # @like.save
+  # redirect_to "/users/#{current_user.id}/posts/#{@post.id}"
+  # end
   def create
     @like = Like.new(like_params)
     redirect_to user_post_path(id: @like.post_id, user_id: @like.author_id) if @like.save
