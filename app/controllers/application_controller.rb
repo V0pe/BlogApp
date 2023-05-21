c600b66fe45ea633622a8f3dedab3d0dcfb8460b
V0pe <dampblew01@gmail.com>
2023-05-21 15:02:40 -0600

    fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :user_data
+  helper_method :user_posts
+  helper_method :current_user
+  def user_data
+    @user_data ||= User.all
+  end
+  def user_posts
+    @user_posts ||= Post.all
+  end
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
@@ -8,7 +17,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
       user_params.permit(:email, :encrypted_password)
     end
-
+    
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:name, :email, :password, :password_confirmation)
     end
